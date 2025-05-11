bff61072f17b18057fc2ee066e84f88b5bab7f4e
Wdboyes13 <willdev2025@outlook.com>
2025-05-11 01:29:21 -0700

    Did the thing to accept Lang arg

--- BMF-ProjC.sh
+++ BMF-ProjC.sh
@@ -3,19 +3,20 @@
 # TODO: Add Language Arg
 
 # Check arguments first!
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo "Usage: $0 <project-name> <source-file.c / source-dir> <project-type> <profile-name>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; || [-z "$4"] then
+    echo "Usage: $0 <project-name> <source-file.c / source-dir> <project-type> <Lang> <profile-name>"
     exit 1
 fi
 PROJ_NAME="$1"
 SOURCE="$2"
 PROJ_TYPE="$3"
+LANG="$4"
 curl -sU
 mkdir "$1"
 cd "$1" || exit 1
 curl -sL https://raw.githubusercontent.com/Wdboyes13/BetterMake/refs/heads/main/bettermake/target/BetterMake.jar -o BetterMake.jar
-if [ -n "$4" ]; then
-    PROF="$4"
+if [ -n "$5" ]; then
+    PROF="$5"
     if [ -f "$HOME/.bmf-profs/$PROF" ]; then
         source "$HOME/.bmf-profs/$PROF"
     else
@@ -27,7 +28,7 @@ MF() {
     mkdir -p rls/{lin,linARM,mac,macARM,win,winARM}
     mkdir -p build/{LINARM,LIN64,MAC64,MACARM,WIN64,WINARM}
     mkdir src
-
+    touch src/$SOURCE
     cat <<EOF >> mk.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <BMF xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -52,7 +53,7 @@ MF() {
     </Compilers>
     <FILE>$PROJ_NAME</FILE>
     <SRC>
-        <LANG>C</LANG>
+        <LANG>$LANG</LANG>
         <Type>MultiFile</Type>
         <FILE>src</FILE>
     </SRC>
@@ -92,7 +93,7 @@ OF() {
     </Compilers>
     <FILE>$PROJ_NAME</FILE>
     <SRC>
-        <LANG>C</LANG>
+        <LANG>$LANG</LANG>
         <Type>OneFile</Type>
         <FILE>$SOURCE</FILE>
     </SRC>
--- Docs/RUNNING.md
+++ Docs/RUNNING.md
@@ -1,11 +1,17 @@
 # To run
 ## To see info on Project Profiles go to [PROFILES.md](PROFILES.md)
 To make a new MultiFile Project  (May request a proxy, hit enter, ignore error)
+Replace <Lang> with OBJC, OBJCPP, C, or CPP
+Replace <project-name> With your project name (Name of project folder & final binary)
+Replace <file.c> with what you want your first file to be named
 ```sh
-curl --noproxy "*" -fsSL https://raw.githubusercontent.com/Wdboyes13/BetterMake/refs/heads/main/BMF-ProjC.sh | bash -s <project-name> x MF <OPTIONAL: profile-name>
+curl --noproxy "*" -fsSL https://raw.githubusercontent.com/Wdboyes13/BetterMake/refs/heads/main/BMF-ProjC.sh | bash -s <project-name> <file.c> MF <Lang> <OPTIONAL: profile-name>
 ```  
 
 To make a new SingleFile Project  (May request a proxy, hit enter, ignore error)
+Replace <Lang> with OBJC, OBJCPP, C, or CPP
+Replace <project-name> With your project name (Name of project folder & final binary)
+Replace <file.c> with what you want your source file to be named
 ```sh
 curl --noproxy "*" -fsSL https://raw.githubusercontent.com/Wdboyes13/BetterMake/refs/heads/main/BMF-ProjC.sh | bash -s <project-name> <file.c> OF <OPTIONAL: profile-name>
 ```  
